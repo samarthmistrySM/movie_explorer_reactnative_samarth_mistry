0fcdb4237ca186769024d986810f9d152acdb3b3
samarthmistrySM <g33ky.sam@gmail.com>
2025-04-24 13:12:24 +0530

    [Samarth]: set up React Native CLI with base navigation and installed dependencies

---
+++ src/screens/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Home;
